6f6a2069afc91f69e1be04412b0e49e450ed049f
aSilverDpad <aSilverDpad@gmail.com>
2017-02-16 21:22:07 +0000

    Changed method to a simpler step by step approach

--- Air_Hockey.py
+++ Air_Hockey.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python2.7
-#
-# Air Hockey Game Ported to Python by aSilverDpad
-#
-# Released under the GNU General Public License
 
-VERSION = '0.1'
+VERSION = "0.2"
 
 try:
     import sys
-    import random
-    import math
     import os
-    import getopt
     import pygame
-    from socket import *
-    from pygame.locals import *
 except ImportError, err:
     print "couldn't load module. %s" % (err)
     sys.exit(2)
 
 def load_png(name): # {{{1
-    '''Load image and return object.
-        looks for image in ./images
-        Returns: image, image.rect.'''
-    fullname = os.path.join('data', name)
+    """ Load image and return image object"""
+    fullname = os.path.join('images', name)
     try:
         image = pygame.image.load(fullname)
-        if image.get_alpha() is None: #Transparency
+        if image.get_alpha() is None:
             image = image.convert()
         else:
             image = image.convert_alpha()
@@ -34,56 +23,52 @@ def load_png(name): # {{{1
         print 'Cannot load image:', fullname
         raise SystemExit, message
     return image, image.get_rect()
-# }}}1
-class Puck(pygame.sprite.Sprite): # {{{1
-    '''A circle shaped hockey puck that will
-    move across the screen when hit by the paddles.
-    Returns: puck object
-    Functions: ... ...
-    Attributes: ... ...'''
 
-    def __init__(self, (xy), vector): #{{{2
+class Puck(pygame.sprite.Sprite): # {{{1
+    '''A Hockey puck that moves around the screen
+    Returns: puck object
+    Functions:
+    Attributes:'''
+
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png('puck.png')
-        screen = pygame.display.get_surface() #TODO: make global? in main()?
-        self.moveable_area = screen.get_rect()
-        self.vector = vector
-        self.hit = 0
+        self.vector = [1,1]
 
 
-
-# }}}1
-class Paddle(pygame.sprite.Sprite): # {{{1
-    '''Movable hockey paddle with which to hit the puck
-    Returns: paddle objects
-    Functions:
-    Attributes:  '''
-
-    def __init__(self, side): # {{{2
-            pygame.sprite.Sprite.__init__(self)
-            self.image, self.rect = load_png('puck.png')
-            screen = pygame.display.get_surface() #TODO: make global?
-            self.moveable_area = screen.get_rect() # devide it in two later
-            self.side = side
-            self.speed = 10
-            self.state = 'still'
-            self.reinit()
-
-# }}}1
-def main(): # {{{1
+def main(): #{{{1
     pygame.init()
-    screen = pygame.display.set_mode((640,480))
-    pygame.display.set_caption('Air Hockey')
+    size = width, height = 800, 500
+    black = 0,0,0
 
-    # Init clock
-    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(size)
 
-    # Event loop
+    pygame.mouse.set_pos(0,0)
+    paddle, paddlerect = load_png('bluepaddle.png')
+    puck = Puck()
+    pucksprite = pygame.sprite.RenderPlain(puck)
+
     while True:
-        clock.tick(60) # 60 fps max
-
         for event in pygame.event.get():
-            if event.type == QUIT:
-                return
+            if event.type == pygame.QUIT: sys.exit()
+
+        puck.rect = puck.rect.move(puck.vector)
+        paddlerect = paddlerect.move(pygame.mouse.get_rel())
+
+        # puck - wall collition
+        if puck.rect.left < 0 or puck.rect.right > width:
+            puck.vector[0] = -puck.vector[0]
+        if puck.rect.top < 0 or puck.rect.bottom > height:
+            puck.vector[1] = -puck.vector[1]
+        # paddle - puck collition
+        if puck.rect.colliderect(paddlerect) == 1:
+            puck.vector[0] = -puck.vector[0]
+
+        screen.fill(black)
+        screen.blit(screen, puck.rect)
+        screen.blit(paddle, paddlerect)
+        pucksprite.update()
+        pucksprite.draw(screen)
+        pygame.display.flip()
 
 if __name__ == '__main__': main()
